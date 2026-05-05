50560b4cd48ff93ab4352aa674c37cb5ad63d509
paulohtolotti <paulohtolotti@alumni.usp.br>
2026-05-05 09:01:47 -0300

    first commit

---
+++ src/main/java/com/pht/rinha_backend_2026/RinhaBackend2026Application.java
@@ -0,0 +1,13 @@
+package com.pht.rinha_backend_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackend2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaBackend2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pht/rinha_backend_2026/RinhaBackend2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pht.rinha_backend_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackend2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
