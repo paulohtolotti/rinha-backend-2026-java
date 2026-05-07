fb696046679a56b46ce848cae74e3ae3ef17e86c
paulohtolotti <paulohtolotti@alumni.usp.br>
2026-05-07 16:27:10 -0300

    feat: criação do serviço de busca inicial

--- src/main/java/com/pht/rinha_backend_2026/services/SearchService.java
+++ src/main/java/com/pht/rinha_backend_2026/services/SearchService.java
@@ -1,6 +1,22 @@
 package com.pht.rinha_backend_2026.services;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class SearchService {
 
+    public SearchService() {
 
+    }
+    /**
+     * Realiza o cálculo de similaridade entre os vetores e retorna o nº de fraudes
+     * @param incomingTransaction Transação vinda do endpoint vetorizada
+     * @return
+     */
+    public int numberOfFrauds(double[] incomingTransaction) {
+        return 1;
+    }
 }
+
+
